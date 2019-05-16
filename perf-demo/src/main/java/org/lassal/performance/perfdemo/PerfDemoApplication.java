1684d922fe169c58ad5c33d102cf9e79f5dd811b
Lassal <lassal@lassal.ubuntu.work>
2019-05-16 06:29:53 -0300

    Start project

---
+++ perf-demo/src/main/java/org/lassal/performance/perfdemo/PerfDemoApplication.java
@@ -0,0 +1,13 @@
+package org.lassal.performance.perfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfDemoApplication.class, args);
+	}
+
+}
---
+++ perf-demo/src/test/java/org/lassal/performance/perfdemo/PerfDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lassal.performance.perfdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerfDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
